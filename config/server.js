c3265fc4ff811f2689121936d9af10e206e2a22c
Herman Adu <herman.adu@hotmail.com>
2022-02-11 20:05:43 +0000

    initial Upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0007e193cf50a59e66695672de95b7d9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8ecf1f76-bf4a-492a-b0e4-83467c9f927b'
+};
